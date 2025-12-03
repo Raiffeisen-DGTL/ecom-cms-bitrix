1ba2fbd8f3741e5b8fadb136410e27a09aa4a837
LUKYANENKO Elizaveta S <elizaveta.lukyanenko@raiffeisen.ru>
2025-12-03 12:36:18 +0000

    Update files to clean SBR

--- lang/en/install/index.php
+++ lang/en/install/index.php
@@ -1,5 +1,1 @@
 <?
-$MESS["PAYMENT_MODULE_NAME"] = "Sberbank card payment";
-$MESS["PAYMENT_MODULE_DESCRIPTION"] = "The payment acceptance module allows you to simplify the connection to the payment gateway of the Sberbank for accepting bank cards of International Payment Systems (Visa, MasterCard, World) on your website";
-$MESS["PAYMENT_PARTNER_NAME"] = "Sberbank";
-$MESS["PAYMENT_PARTNER_URI"] = "http://sberbank.ru/";
